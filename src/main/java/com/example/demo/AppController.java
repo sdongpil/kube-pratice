40bba89ccde650335a37c35a7886585cd2d8914b
sdongpil <pildong94@naver.com>
2025-03-27 20:28:33 +0900

    docs: 패키기 정리

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,19 +1,12 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
-    @Value("${MY_ACCOUNT}")
-    private String myAccount;
-
-    @Value("${MY_PASSWORD}")
-    private String myPassword;
-
     @GetMapping("/")
     public String home() {
-        return "myAccount: " + myAccount + ", myPassword: " + myPassword;
+        return "Hello";
     }
 }
